c6b56477fb1654d00ff0ce722580cdafa11ffd54
mmm <margaret.m.matson@gmail.com>
2022-12-10 17:18:39 -0600

    imported required modules and packages, set up middleware for parsing json and urlencoded data, and created routes to get and post notes

--- server.js
+++ server.js
@@ -1,21 +1,84 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { readFromFile } = require('./helpers/fsUtils');
+const uuid = require('./helpers/uuid');
+
+const { clog } = require('./middleware/clog');
 
 const app = express();
 const PORT = 3001;
+
+// Import custom middleware, "clog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 // GET Route for home page
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET Route for retrieving all of the notes - renders notes to page
+app.get('/api/notes', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST request to add a note - adds a note to the page
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        // Obtain existing notes
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new note
+                parsedNotes.push(newNote);
+
+                // Write updated notes back to the file
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+});
+
 app.listen(PORT, () =>
-  console.log(`Note taker app listening at http://localhost:${PORT}`)
+    console.log(`Note taker app listening at http://localhost:${PORT}`)
 );
