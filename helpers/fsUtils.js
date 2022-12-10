41101dda9ce0435c423ca10703913d1c4c60d439
mmm <margaret.m.matson@gmail.com>
2022-12-10 17:06:33 -0600

    added fsUtils file to include promise version of fs.readFile

---
+++ helpers/fsUtils.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const util = require('util');
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+
+module.exports = {readFromFile};
